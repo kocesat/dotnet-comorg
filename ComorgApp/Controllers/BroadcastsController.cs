eda36535a5f69b676c6954b87b7e42110e8525a6
Esat Koç <esat.koc@metu.edu.tr>
2021-11-14 11:30:54 +0300

    TinyMCE Editor amended to broadcast textareas

---
+++ ComorgApp/Controllers/BroadcastsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ComorgApp.Data;
+using ComorgApp.Entities;
+
+namespace ComorgApp.Controllers
+{
+    public class BroadcastsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BroadcastsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Broadcasts
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Broadcasts
+                        .OrderByDescending(b => b.Created)
+                        .ToListAsync());
+        }
+
+    }
+}
